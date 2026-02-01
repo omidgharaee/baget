0f6c5188bf42f2140fe436984c6ad259e521111e
OmidGharaee <omid.gharaee@hotmail.com>
2026-02-01 14:29:32 +0330

    remove Humanizer

--- src/BaGet.Web/Extensions/RazorExtensions.cs
+++ src/BaGet.Web/Extensions/RazorExtensions.cs
@@ -1,12 +1,23 @@
-﻿using Humanizer;
-
 namespace BaGet.Web
 {
     public static class RazorExtensions
     {
         public static string ToMetric(this long value)
         {
-            return ((double) value).ToMetric();
+            if (value < 1000)
+                return value.ToString();
+
+            string[] suffixes = { "K", "M", "B", "T" };
+            double tempValue = value;
+            int suffixIndex = -1;
+
+            while (tempValue >= 1000 && suffixIndex < suffixes.Length - 1)
+            {
+                tempValue /= 1000;
+                suffixIndex++;
+            }
+
+            return $"{tempValue:0.#}{suffixes[suffixIndex]}";
         }
     }
 }
