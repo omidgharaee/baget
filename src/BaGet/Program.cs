b803d6f61c8ea4f7c97cafc1d88de9e1ac6a48dc
OmidGharaee <omid.gharaee@hotmail.com>
2026-02-01 14:41:45 +0330

    Top-level statements

--- src/BaGet/Program.cs
+++ src/BaGet/Program.cs
@@ -1,84 +1,72 @@
 using System;
-using System.Threading.Tasks;
 using BaGet.Core;
 using BaGet.Web;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
-namespace BaGet
+// ----------------------
+// Top-level statements
+// ----------------------
+
+var builder = WebApplication.CreateBuilder(args);
+
+var root = Environment.GetEnvironmentVariable("BAGET_CONFIG_ROOT");
+if (!string.IsNullOrEmpty(root))
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            if (!host.ValidateStartupOptions())
-            {
-                return;
-            }
-
-            var app = new CommandLineApplication
-            {
-                Name = "baget",
-                Description = "A light-weight NuGet service",
-            };
-
-            app.HelpOption(inherited: true);
-
-            app.Command("import", import =>
-            {
-                import.Command("downloads", downloads =>
-                {
-                    downloads.OnExecuteAsync(async cancellationToken =>
-                    {
-                        using (var scope = host.Services.CreateScope())
-                        {
-                            var importer = scope.ServiceProvider.GetRequiredService<DownloadsImporter>();
-
-                            await importer.ImportAsync(cancellationToken);
-                        }
-                    });
-                });
-            });
-
-            app.Option("--urls", "The URLs that BaGet should bind to.", CommandOptionType.SingleValue);
-
-            app.OnExecuteAsync(async cancellationToken =>
-            {
-                await host.RunMigrationsAsync(cancellationToken);
-                await host.RunAsync(cancellationToken);
-            });
-
-            await app.ExecuteAsync(args);
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host
-                .CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((ctx, config) =>
-                {
-                    var root = Environment.GetEnvironmentVariable("BAGET_CONFIG_ROOT");
-
-                    if (!string.IsNullOrEmpty(root))
-                    {
-                        config.SetBasePath(root);
-                    }
-                })
-                .ConfigureWebHostDefaults(web =>
-                {
-                    web.ConfigureKestrel(options =>
-                    {
-                        // Remove the upload limit from Kestrel. If needed, an upload limit can
-                        // be enforced by a reverse proxy server, like IIS.
-                        options.Limits.MaxRequestBodySize = null;
-                    });
-
-                    web.UseStartup<Startup>();
-                });
-        }
-    }
+    builder.Configuration.SetBasePath(root);
 }
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Remove upload limit
+    options.Limits.MaxRequestBodySize = null;
+
+    if (builder.Environment.IsProduction())
+    {
+        options.ListenAnyIP(80);
+    }
+});
+
+var host = builder.Build();
+
+if (!host.ValidateStartupOptions())
+{
+    return;
+}
+
+var appCli = new CommandLineApplication
+{
+    Name = "baget",
+    Description = "A light-weight NuGet service"
+};
+
+appCli.HelpOption(inherited: true);
+
+appCli.Command("import", import =>
+{
+    import.Command("downloads", downloads =>
+    {
+        downloads.OnExecuteAsync(async cancellationToken =>
+        {
+            using var scope = host.Services.CreateScope();
+            var importer = scope.ServiceProvider.GetRequiredService<DownloadsImporter>();
+
+            await importer.ImportAsync(cancellationToken);
+        });
+    });
+});
+
+appCli.Option("--urls", "The URLs that BaGet should bind to.", CommandOptionType.SingleValue);
+
+appCli.OnExecuteAsync(async cancellationToken =>
+{
+    await host.RunMigrationsAsync(cancellationToken);
+
+    await host.RunAsync(cancellationToken);
+});
+
+await appCli.ExecuteAsync(args);
